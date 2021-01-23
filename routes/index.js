560fe5cddcc56f5e0bf4cb2ff5cbf765a52f8886
siddharth chaudhary <siddharthchaudhary360@gmail.com>
2021-01-23 05:54:52 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // res.render('index')
   // console.log("hello")
 
-  // // Import the Google Cloud client library and create a client
+  // // [START bigquery_get_table]
+  // // Import the Google Cloud client library
   // const { BigQuery } = require('@google-cloud/bigquery');
   // const options = {
   //   keyFilename: '/home/siddharthchaudhary/Documents/Sunny75-d7098066311b.json',
@@ -13,55 +14,118 @@ router.get('/', (req, res) => {
   // };
   // const bigquery = new BigQuery(options);
 
-  // async function createDataset() {
-  //   // Creates a new dataset named "my_dataset".
+  // async function getTable() {
+  //   // Retrieves table named "my_table" in "my_dataset".
 
   //   /**
-  //    * TODO(developer): Uncomment the following lines before running the sample.
+  //    * TODO(developer): Uncomment the following lines before running the sample
   //    */
   //   const datasetId = "analytics_258212991";
+  //   const tableId = "events_20210119";
 
-  //   // Specify the geographic location where the dataset should reside
+  //   // Retrieve table reference
+  //   const dataset = bigquery.dataset(datasetId);
+  //   const [table] = await dataset.table(tableId).get();
+
+  //   console.log('Table:');
+  //   // console.log(table.metadata.tableReference);
+  //   console.log(table);
+  // }
+  // getTable();
+  // // [END bigquery_get_table]
+
+  // // [START bigquery_create_job]
+  // // Import the Google Cloud client library and create a client
+  // const { BigQuery } = require("@google-cloud/bigquery");
+  // const options = {
+  //   keyFilename: "/home/siddharthchaudhary/Documents/Sunny75-d7098066311b.json",
+  //   projectId: "sunny75-5dee6",
+  // };
+  // const bigquery = new BigQuery(options);
+
+  // async function createJob() {
+  //   // Run a BigQuery query job.
+
+  //   // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/Job
   //   const options = {
-  //     location: 'US',
+  //     // Specify a job configuration to set optional job resource properties.
+  //     configuration: {
+  //       query: {
+  //         query: `SELECT count(DISTINCT(device.vendor_id)) AS user_count FROM \`sunny75-5dee6.analytics_258212991.events_20210119\` LIMIT 1000`,
+  //         useLegacySql: false,
+  //       },
+  //       // labels: { "example-label": "example-value" },
+  //     },
   //   };
 
-  //   // Create a new dataset
-  //   const [dataset] = await bigquery.createDataset(datasetId, options);
-  //   console.log(`Dataset ${dataset.id} created.`);
+  //   // Make API request.
+  //   const response = await bigquery.createJob(options);
+  //   const job = response[0];
+
+  //   // Wait for the query to finish
+  //   const [rows] = await job.getQueryResults(job);
+
+  //   // Print the results
+  //   console.log("Rows:", rows);
+  //   rows.forEach((row) => console.log(row));
   // }
-  // createDataset();
+  // // [END bigquery_create_job]
+  // createJob();
 
-
-  // [START bigquery_get_table]
-  // Import the Google Cloud client library
-  const { BigQuery } = require('@google-cloud/bigquery');
+  const { BigQuery } = require("@google-cloud/bigquery");
   const options = {
-    keyFilename: '/home/siddharthchaudhary/Documents/Sunny75-d7098066311b.json',
-    projectId: 'sunny75-5dee6',
+    keyFilename: "/home/siddharthchaudhary/Documents/Sunny75-d7098066311b.json",
+    projectId: "sunny75-5dee6",
   };
   const bigquery = new BigQuery(options);
 
-  async function getTable() {
-    // Retrieves table named "my_table" in "my_dataset".
+  async function listTables() {
+    // Lists tables in 'my_dataset'.
 
     /**
-     * TODO(developer): Uncomment the following lines before running the sample
+     * TODO(developer): Uncomment the following lines before running the sample.
      */
+    // const datasetId = "analytics_258212991";
+
+    // // List all tables in the dataset
+    // const [tables] = await bigquery.dataset(datasetId).getTables();
+
+    // console.log("Tables:");
+    // tables.forEach(async (table1) => {
+    //   console.log(table1.id);
+
+    //   const srcDatasetId = "analytics_258212991";
+    //   // const srcTableId = "events_20210119";
+    //   const srcTableId = table1.id;
+    //   const destDatasetId = "sunny75_dataset";
+    //   const destTableId = "sunny75_table";
+
+    //   // Copy the table contents into another table
+    //   const [job] = await bigquery
+    //     .dataset(srcDatasetId)
+    //     .table(srcTableId)
+    //     .copy(bigquery.dataset(destDatasetId).table(destTableId));
+
+    //   console.log(`Job ${job.id} completed.`);
+
+    //   // Check the job's status for errors
+    //   const errors = job.status.errors;
+    //   if (errors && errors.length > 0) {
+    //     throw errors;
+    //   }
+    // });
+
     const datasetId = "analytics_258212991";
-    const tableId = "events_20210119";
-
-    // Retrieve table reference
     const dataset = bigquery.dataset(datasetId);
-    const [table] = await dataset.table(tableId).get();
 
-    console.log('Table:');
-    // console.log(table.metadata.tableReference);
-    console.log(table);
+    dataset.getModels().then((data) => {
+      const models = data[0];
+      console.log("Models:", models);
+      models.forEach((model) => console.log(model.metadata));
+    });
   }
-  getTable();
-  // [END bigquery_get_table]
+  // [END bigquery_list_tables]
+  listTables();
+});
 
-})
-
-module.exports = router
+module.exports = router;
